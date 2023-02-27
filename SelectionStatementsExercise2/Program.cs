5f0674508f641c147d08d16a887721489c329465
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-26 19:12:48 -0700

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello! What is your favorite subject in school? Please type here:");
         }
     }
 }
