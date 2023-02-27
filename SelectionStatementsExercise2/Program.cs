e4ce61ac631477f1b3aa3a6e0169461438f1c7b2
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-26 19:29:26 -0700

    Created switch statement

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -5,6 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! What is your favorite subject in school? Please type here:");
+            string favSubject = Console.ReadLine();
+            
+            switch (favSubject.ToLower())
+            {
+                case ("math"):
+                    Console.WriteLine($"Wonderful! I also really like math.");
+                    break;
+                case ("english"):
+                    Console.WriteLine($"Great! I also like English. Reading great literature can be so fun!");
+                    break;
+                case ("science"):
+                    Console.WriteLine($"That is fantastic! My dad is a scientist.");
+                    break;
+                case ("geography"):
+                    Console.WriteLine($"Very interesting! I was always terrible with geography.");
+                    break;
+                case ("computer programming"):
+                    Console.WriteLine($"Aha! That is my favorite as well!");
+                    break;
+                default:
+                    Console.WriteLine("Wow! I don't think that I have ever heard of that class!");
+                    break;
+            }
         }
     }
 }
