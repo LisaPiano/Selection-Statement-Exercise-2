d6e7f65be47f41e0d8d6f6fad660a83741c85824
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-28 16:40:16 -0700

    added more cases and finished

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -10,12 +10,15 @@
             switch (favSubject.ToLower())
             {
                 case ("math"):
+                case ("calculus"):
                     Console.WriteLine($"Wonderful! I also really like math.");
                     break;
                 case ("english"):
                     Console.WriteLine($"Great! I also like English. Reading great literature can be so fun!");
                     break;
                 case ("science"):
+                case ("biology"):
+                case ("chemistry"):
                     Console.WriteLine($"That is fantastic! My dad is a scientist.");
                     break;
                 case ("geography"):
@@ -25,7 +28,7 @@
                     Console.WriteLine($"Aha! That is my favorite as well!");
                     break;
                 default:
-                    Console.WriteLine("Wow! I don't think that I have ever heard of that class!");
+                    Console.WriteLine("Wow! I don't think that I have ever taken that class!");
                     break;
             }
         }
